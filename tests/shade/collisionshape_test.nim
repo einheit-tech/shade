612798e6a55b4e01f48c8f1408dddbb3412f0a3e
Avahe Kellenberger <avahe@protonmail.ch>
2022-01-05 21:50:08 -0500

    Updated tests.

--- tests/shade/collisionshape_test.nim
+++ tests/shade/collisionshape_test.nim
@@ -29,6 +29,24 @@ describe "CollisionShape tests":
       let expected = normalize(circleB.center - circleA.center + aToB)
       assertEquals(projection, expected)
 
+    it "projects the circle correctly onto a horizontal axis":
+      let
+        axis = vector(1, 0)
+        projection = circleA.projectOnAxis(VECTOR_ZERO, axis)
+      # circle.center.x - circle.radius
+      assertEquals(projection.x, 74)
+      # circle.center.x + circle.radius
+      assertEquals(projection.y, 174)
+
+    it "projects the circle correctly onto a vertical axis":
+      let
+        axis = vector(0, 1)
+        projection = circleA.projectOnAxis(VECTOR_ZERO, axis)
+      # circle.center.y - circle.radius
+      assertEquals(projection.x, 206)
+      # circle.center.y + circle.radius
+      assertEquals(projection.y, 306)
+
     it "getPolygonProjectionAxes":
       let projections = rectPoly.getPolygonProjectionAxes()
 
