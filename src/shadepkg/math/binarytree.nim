9c1d689cb097df70f47286cd82f47786c252502b
Avahe Kellenberger <avahe@protonmail.ch>
2022-01-13 22:13:18 -0500

    Added binary tree postorder traversal.

--- src/shadepkg/math/binarytree.nim
+++ src/shadepkg/math/binarytree.nim
@@ -1,10 +1,46 @@
 type BinaryTree*[T] = ref object 
   left, right: BinaryTree[T]
-  data: T
+  value: T
 
-proc newNode*[T](data: T): BinaryTree[T] =
+proc newNode*[T](value: T): BinaryTree[T] =
   new(result)
-  result.data = data
+  result.value = value
+
+template isLeafNode*[T](node: BinaryTree[T]): bool =
+  node.left.isNil and node.right.isNil
+
+iterator preorder*[T](this: BinaryTree[T]): BinaryTree[T] =
+  ## Preorder traversal of a binary tree.
+  ## Left, root, then right (depth-first search, top to bottom).
+  var stack: seq[BinaryTree[T]] = @[this]
+  while stack.len > 0:
+    var n = stack.pop()
+    while n != nil:
+      yield n
+      stack.add(n.right)  
+      n = n.left          
+
+template doWhile(condition, body: untyped): untyped =
+  body
+  while condition:
+    body
+
+iterator postorder*[T](this: BinaryTree[T]): BinaryTree[T] =
+  ## Postorder traversal of a binary tree.
+  ## Left, root, then right (depth-first search, bottom to top).
+  var stack: seq[BinaryTree[T]] = @[this]
+  doWhile stack.len > 0:
+    let n = stack.pop()
+    if n.isLeafNode():
+      yield n
+    else:
+      if n.right != nil:
+        stack.add(n.right)
+
+      if n.left != nil:
+        stack.add(n.left)
+      
+  yield this
 
 proc add*[T](this: var BinaryTree[T], n: BinaryTree[T]) =
   ## Inserts a node into the tree.
@@ -13,10 +49,10 @@ proc add*[T](this: var BinaryTree[T], n: BinaryTree[T]) =
   else:
     var it = this
     while it != nil:
-      # Compare the data items,
-      # using the generic `cmp` proc that works for any type that has a `==` and `<` operator
-      var c = cmp(it.data, n.data)
-      if c < 0:
+      # Compare the value items,
+      # using the generic `cmp` proc that works for any type that has a `==` and `<` operator.
+      # In the future we will enforce this with concepts, when they are ready.
+      if cmp(it.value, n.value) > 0:
         if it.left == nil:
           it.left = n
           return
@@ -27,20 +63,12 @@ proc add*[T](this: var BinaryTree[T], n: BinaryTree[T]) =
           return
         it = it.right
 
-proc add*[T](this: var BinaryTree[T], data: T) =
-  this.add(newNode(data))
+template add*[T](this: var BinaryTree[T], value: T) =
+  this.add(newNode(value))
 
-iterator preorder*[T](this: BinaryTree[T]): T =
-  ## Preorder traversal of a binary tree.
-  # This uses an explicit stack,
-  # which is more efficient than a recursive iterator factory.
-  var stack: seq[BinaryTree[T]] = @[this]
-  while stack.len > 0:
-    var n = stack.pop()
-    while n != nil:
-      yield n.data
-      # Push right subtree onto the stack
-      stack.add(n.right)  
-      # Then follow the left pointer
-      n = n.left          
+# TODO:
+# See:
+# https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+# https://www.geeksforgeeks.org/level-order-tree-traversal/
+# https://www.azurefromthetrenches.com/introductory-guide-to-aabb-tree-collision-detection/
 
