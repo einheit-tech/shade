e1580ca0b5eebf8ec065a5bb12bad78e1f880525
Avahe Kellenberger <avahe@protonmail.ch>
2022-09-13 21:44:44 -0400

    Implemented SpaceEvenly.

--- src/shadepkg/ui/alignment/alignment_space_evenly.nim
+++ src/shadepkg/ui/alignment/alignment_space_evenly.nim
@@ -2,12 +2,17 @@ import ../ui_component
 
 from alignment_center import alignCrossAxis
 
+import std/heapqueue
+
 template alignMainAxis(this: UIComponent, axis: static StackDirection) =
+  mixin push
+
   let totalAvailableLen = this.len() - this.totalPaddingAndBorders(axis)
   let maxChildLen = determineDynamicChildLenMainAxis(this, axis)
 
   var
     totalChildrenLen: float
+    margins = initHeapQueue[float]()
     prevChild: UIComponent
 
   # Calculate the total length all children use up
@@ -16,17 +21,44 @@ template alignMainAxis(this: UIComponent, axis: static StackDirection) =
       childPixelLen = pixelLen(this, child, axis)
       childLen = if childPixelLen > 0: childPixelLen else: maxChildLen
 
+    if prevChild == nil:
+      margins.push(-child.startMargin)
+    else:
+      margins.push(-max(child.startMargin, prevChild.endMargin))
+
     totalChildrenLen += childLen
     prevChild = child
 
+  if prevChild != nil:
+    margins.push(-prevChild.endMargin)
+
   # Set child positions and sizes
   prevChild = nil
-  let remainingSpace = totalAvailableLen - totalChildrenLen
-  # TODO: Check when remainingSpace is negative
-  let gap: float = if remainingSpace < 0: 0.0 else: remainingSpace / float(this.children.len + 1)
+
+  # Calculate the gap size and remaining space in the parent
+  var
+    remainingSpace = totalAvailableLen - totalChildrenLen
+    i = this.children.len + 1
+    gap =
+      if remainingSpace < 0:
+        0.0
+      else:
+        remainingSpace / float(i)
+
+  while margins.len > 0:
+    let margin = -margins.pop()
+    if margin <= gap:
+      break
+
+    remainingSpace -= margin
+    i -= 1
+    gap =
+      if remainingSpace < 0:
+        0.0
+      else:
+        remainingSpace / float(i)
 
   var childStart: float = this.boundsStart + this.startPadding + this.borderWidth
-
   for child in this.children:
     let
       childPixelLen = pixelLen(this, child, axis)
